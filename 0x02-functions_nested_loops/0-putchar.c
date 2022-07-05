1ea24665d74a7a50e0e5657eec1775d71074874e
Eugene Kankam <ekankam101@gmail.com>
2022-07-05 23:42:55 +0000

    lets get started

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * program that prints
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
