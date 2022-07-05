012ec2c83ed3ac9bde1994f9d0672bc4fcf3eae3
Eugene Kankam <ekankam101@gmail.com>
2022-07-05 04:15:57 +0000

    deep thinking

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,18 @@ int main(void)
 	int i;
 
 	i = 0;
-	while (i <= 9)
+	while (i < 9)
 	{
 		putchar(i + '0');
-		if (i != 9)
-		{
 		putchar(',');
 		putchar(' ');
 		i++;
+		if (i == 9)
+		{
+			putchar('\n');
+			i++;
 		}
 	}
-	putchar('\n');
 	return (0);
 }
 
