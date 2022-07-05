5073e4b950502289c3e665e12c4ada652045f3e7
Eugene Kankam <ekankam101@gmail.com>
2022-07-05 01:09:48 +0000

    while loop with putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	ch = 'a';
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+	ch = 'A';
+
+		while (ch <= 'Z')
+		{
+			putchar(ch);
+			ch++;
+		}
+	putchar('\n');
+}
