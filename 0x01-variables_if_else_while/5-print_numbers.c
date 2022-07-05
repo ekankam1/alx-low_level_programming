0330ccb5b4b276d964091977e72ebe961d86ad5f
Eugene Kankam <ekankam101@gmail.com>
2022-07-05 01:45:56 +0000

    printing base 10 single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
