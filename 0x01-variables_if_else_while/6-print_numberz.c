e850450dcc2b9055fa53d9b2bdd77ef18bc261bc
Eugene Kankam <ekankam101@gmail.com>
2022-07-05 02:23:26 +0000

    putchar without char variable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		int putchar(int i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
