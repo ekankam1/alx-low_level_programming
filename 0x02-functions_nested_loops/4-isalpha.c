d75ef1d328ebe2a3ee62141bd4d8d6f40d49d927
Eugene Kankam <ekankam101@gmail.com>
2022-07-06 03:13:54 +0000

    is alpha?

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - check for alphabetic character and case
+ *
+ * @c: input as integer
+ *
+ * Return: 1 if c is a letter or 0 if otherwise
+ */
+int _isalpha(int c);
+{
+	if (((c >= 65) && (c <= 90)) || ((c >= 97) && (c <= 122)))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
