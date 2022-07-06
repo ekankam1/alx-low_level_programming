cf56dc3dcd3b53736a7eaa00b283e333488c6774
Eugene Kankam <ekankam101@gmail.com>
2022-07-06 03:54:28 +0000

    find absolute of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _abs - compute absolute value of int
+ *
+ * @int: input integer
+ *
+ * Return: int
+ */
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int * -1;
+	}
+		else
+		{
+			if (int > 0)
+			{
+				int;
+			}
+			else
+			{
+				int = 0;
+			}
+		}
+}
