a782a0333b72adb7cd2ce70a626a368fa3dc1311
Eugene Kankam <ekankam101@gmail.com>
2022-07-06 01:52:47 +0000

    one touch

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
