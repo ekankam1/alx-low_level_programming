0bda5e99c4a6eede85fc914e34cdb8d900237fc1
Eugene Kankam <ekankam101@gmail.com>
2022-07-08 03:22:20 +0000

    print with exceptions

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - check code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers from 0-9 excluding 2 and 4
+ *
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while ((i <= 57) && (i != (50 || 52)))
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
