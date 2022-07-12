52a9c8ac662bb36205566aa493ba9638164a0dc9
Eugene Kankam <ekankam101@gmail.com>
2022-07-12 05:36:19 +0000

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
  */
 void rev_string(char *s)
 {
-	int count = 0 , i, j;
+	int count = 0, i, j;
 	char *str, temp;
 
 	while (count >= 0)
@@ -22,7 +22,7 @@ void rev_string(char *s)
 		for (j = i + 1; j > 0; j--)
 		{
 			temp = *(str + j);
-			*(str + j) = *(str + (j-1));
+			*(str + j) = *(str + (j - 1));
 			*(str + (j - 1)) = temp;
 		}
 	}
