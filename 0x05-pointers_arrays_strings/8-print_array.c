a6d64a2506083a4c048b7f53c99ce310551454bc
Eugene Kankam <ekankam101@gmail.com>
2022-07-12 14:44:13 +0000

    learning alot from pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers
+ *
+ * @a: input pointer to an integer
+ *
+ * @n: input an integer
+ *
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to by src
+ *
+ * @dest: input pointer to character
+ *
+ * @src: input point to character
+ *
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+		{
+			break;
+		}
+		count++;
+
+	}
+	return (dest);
+}
