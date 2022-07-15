20dd358240600e5dae14006a12607ee455c5d27c
Eugene Kankam <ekankam101@gmail.com>
2022-07-15 02:37:38 +0000

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,18 +11,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	long unsigned int i = 0;
-	long unsigned int j;
-        char finalstring[50];
+	 unsigned int i = 0;
+	 unsigned int j;
 
 	for (i = 0; i <= strlen(dest); i++)
 	{
-		finalstring[i] = dest[i];
+		dest[i] = dest[i];
 	}
 	for (j = (strlen(dest) + 1); j <= strlen(src); j++)
 	{
-		finalstring[j] = src[j];
+		dest[j] = src[j];
 	}
-	dest = finalstring;
 	return (dest);
 }
