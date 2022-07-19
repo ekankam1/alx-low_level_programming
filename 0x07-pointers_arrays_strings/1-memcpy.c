72901e6df80e0398dba3d9162c98707cf481a04f
Eugene Kankam <ekankam101@gmail.com>
2022-07-19 02:18:46 +0000

    Complete

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n--)
-	{
-		*dest = *src;
-		src++;
-		dest++;
-	}
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
