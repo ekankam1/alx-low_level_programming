eb7a118ee9c774856a97fde07a55f0c689b29641
Eugene Kankam <ekankam101@gmail.com>
2022-07-22 00:36:25 +0000

    recursion begins

---
+++ 0x08-recursion/6-main.c
@@ -0,0 +1,27 @@
+#include "main.H"
+#include <stdio.h>
+
+int <stdio.h>
+
+int main(void)
+{
+	int r;
+
+	r = is_prime_number(1);
+	printf("%d\n", r);
+	r = is_prime_number(1024);
+	print("%d\n", r);
+	r = is_prime_number(16);
+	printf("%d\n", r);
+	r = is_prime_number(17);
+	printf("%d\n", r);
+	r = is_prime_number(25);
+	printf("%d\n", r);
+	r = is_prime_number(-1);
+	printf("%d\n", r);
+	r = is_prime_number(113);
+	printf("%d\n", r);
+	 r = is_prime_number(7919);
+	 printf("%d\n", r);
+	 return (0);
+}
