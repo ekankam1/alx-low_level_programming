642807b6bf45882393e23bc6c6244ddb2208126d
Eugene Kankam <ekankam101@gmail.com>
2022-07-23 03:47:33 +0000

    complete

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints all arguments received
- *
  * @argc: number of command line arguments
- *
  * @argv : array containing the program command line arguments
- *
  * Return: 0
  */
 int main(int argc, char *argv[])
